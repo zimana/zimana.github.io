a190d335327c37ad3c25ac634959bd8c3f4fd9cf
Pierre DeBois <pdebois@zimana.com>
2018-03-18 14:50:59 -0500

    Add files via upload

---
+++ nodetestMarch17.js
@@ -0,0 +1,3 @@
+console.log("test comment for atom runner.");
+console.log("second line for test.");
+console.log("The capability for using Atom Runner was successfully tested on Saturday March 17th, 11:23am.");
