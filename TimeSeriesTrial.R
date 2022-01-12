02aea640ccec0ecfd4814ef8a1b7e23e5bf21f06
Pierre DeBois <pdebois@zimana.com>
2022-01-12 17:39:12 -0600

    Minor update
    Review again Jan 12th

--- TimeSeriesTrial.R
+++ TimeSeriesTrial.R
@@ -3,7 +3,9 @@
 #This is a simple time series with random noise added
 set.seed(123)
 t <- seq(from = 1, to = 200, by = 1) + 10 + rnorm(100, sd = 7)
+# Plot your time series
 plot(t)
+#
 #Four arguments are associated with ts – data, start, end and frequency. 
 #The data argument is the data itself (a vector or matrix). 
 #The start and end arguments allow us to provide a start date and end date for the series. 
